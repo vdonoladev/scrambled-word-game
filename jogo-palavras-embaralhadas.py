30bfddadb5476cad4ed41cf0fb0e2b4efe55a818
vdonoladev <victordonolaf@gmail.com>
2020-10-19 08:23:56 -0300

    Updates

--- jogo-palavras-embaralhadas.py
+++ jogo-palavras-embaralhadas.py
@@ -4,16 +4,15 @@ attempt = 7
 words = 'TEST', 'PROGRAM', 'SHOW', 'EGG', 'LINUX', 'PYTHON', 'JAVASCRIPT', 'CAR', 'FERRARI', 'CAMARO', 'WHITE', 'SEARCH ', 'COMPUTING'
 drawn = random.choice(words)
 while attempt != 0:
-	shuffles = random.sample(drawn, len(drawn))
-	join_scrambled_word = ''.join(shuffles)
-	print(join_scrambled_word)
-	print("="*20)
-	tent = input("Type the word: ").upper()
-	if tent == drawn:
-		print("Congratulations you won!")
-		break
-	else:
-		attempt -= 1
-		print(f"You missed! Remaining attempts: {attempt}")
-		print("="*20)
-		
+    shuffles = random.sample(drawn, len(drawn))
+    join_scrambled_word = ''.join(shuffles)
+    print(join_scrambled_word)
+    print("="*20)
+    tent = input("Type the word: ").upper()
+    if tent == drawn:
+        print("Congratulations you won!")
+        break
+    else:
+        attempt -= 1
+        print(f"You missed! Remaining attempts: {attempt}")
+        print("="*20)
